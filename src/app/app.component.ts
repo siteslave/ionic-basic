f5776731656cd6b2b22bb8630ade639116b6f44c
Satit Rianpit <rianpit@gmail.com>
2016-10-30 13:04:19 +0700

    success menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,18 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, Nav } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 
 import { HomePage } from '../pages/home/home';
-
+import { InputSelectPage } from '../pages/input-select/input-select'
 
 @Component({
-  template: `<ion-nav [root]="rootPage"></ion-nav>`
+  templateUrl: 'app.html' 
 })
 export class MyApp {
+  @ViewChild(Nav) nav: Nav;
+
   rootPage = HomePage;
+  pages: Array<{ title: string, component: any }>;
 
   constructor(platform: Platform) {
     platform.ready().then(() => {
@@ -18,5 +21,16 @@ export class MyApp {
       StatusBar.styleDefault();
       Splashscreen.hide();
     });
+
+    this.pages = [
+      { title: 'Home', component: HomePage },
+      { title: 'Login', component: InputSelectPage }
+    ];
+    
   }
+
+  openPage(page) {
+    this.nav.setRoot(page.component)
+  }
+  
 }
